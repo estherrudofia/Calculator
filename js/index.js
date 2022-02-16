4d686f00e7b14985faf5a2a60471efc0962bc54d
Estherr <queenstarry247@gmail.com>
2022-02-16 10:21:45 +0100

    fixing operators

--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
 let buttons = document.querySelectorAll('.btn');
 let display = document.getElementById('display');
 let label = document.getElementById('label');
-let operators = document.getElementsByClassName('operators');
-
-
 
 
 const operations = (e) => {
@@ -22,19 +19,54 @@ const operations = (e) => {
         });
     }
     else if(btnValue == '=') {
-        display.value = eval(inputValue);
+        display.value = eval(inputValue).toLocaleString();
         label.value = '';
         if (display.value == 'undefined') {
             display.value = '';
         }
     }
-    // else if (display.value.length == 1) {
-    //     operators.value = '';
-    // }
+
     else{
+        
+        // To prevent operator at the start od display
+        if(display.value.length  == 0 && /^[+/*]/.test(btnValue)) return;
+        
+        const lengths = display.value.length;
+        // To stop multiple operators from concatinating
+        if(/[+/*-]/.test(display.value[lengths - 1]) && /[+/*-]/.test(btnValue)) {
+
+            display.value = display.value.slice(0, lengths-1) + btnValue;
+
+            if(display.value.length < 2 && /^[+/*]/.test(btnValue)) {
+                display.value = '-';
+                return;
+            };
+           
+            return;
+            
+        }
+
+        if ('selectionStart' in display) {
+            // check whether some text is selected in display
+            if (display.selectionStart != display.selectionEnd) {
+                let newText = display.value.substring(0, display.SelectionStart) + 
+                "[start]" + display.value.substring(display.selectionStart, display.selectionEnd) + "[end]" + 
+                display.value.substring(display.selectionEnd);
+                display.value = newText;
+            }
+        }else {
+            let textRange = document.selection.createRange();
+            let rangeParent = textRange.parentElement();
+            if (rangeParent === display) {
+                textRange.text = "[start]" + textRange + "[end]";
+            }
+
+        }
+
         display.value += btnValue;
-        label.value = eval(display.value);
-        console.log(label.value);
+        label.value = eval(display.value).toLocaleString();
+        
+            
     }
 };
 
